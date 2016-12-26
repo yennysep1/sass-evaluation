a15a439f6c9d54b44a05ea702ed7822769ed1d46
Your Name <you@example.com>
2016-12-26 11:21:59 -0300

    subiendo sass-evaluation

---
+++ js/script.js
@@ -0,0 +1,9 @@
+  //ACORDEON LATERAL
+  $(document).ready(function(){
+  		  // MENU MOVIL
+	  	$(".button-collapse").sideNav();
+	  	  //ACORDEON LATERAL
+	    $('.collapsible').collapsible();
+
+  });
+
